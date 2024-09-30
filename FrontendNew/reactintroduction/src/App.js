e6b7f79c55885eefe7b305dc66ba6385cd5e98d7
DESKTOP-AJU7NQC\Ivan Jozina <ivan.jaman7@gmail.com>
2024-09-30 08:32:30 +0200

    Works

--- FrontendNew/reactintroduction/src/App.js
+++ FrontendNew/reactintroduction/src/App.js
@@ -1,77 +1,81 @@
-import './App.css';
-import { Button } from './Button';
-import { Table } from './Table';
-import { InputBar } from './InputBar';
-import { useState, useEffect  } from 'react';
-import axios from 'axios';
+import "./App.css";
+import { Button } from "./Button";
+import { Table } from "./Table";
+import { InputBar } from "./InputBar";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [user, setUser] = useState({firstName: '', lastName: '' , sex: '', dateOfBirth: ''});
+  const [user, setUser] = useState({ firstName: "", lastName: "" });
   const [users, setUsers] = useState([]);
   const [editId, setEditId] = useState(null);
 
-  useEffect(() => {
+  function getUsers() {
     axios
-      .get('https://localhost:7267/User/GetAll')
-      .then(response => {
-          console.log(response.data)
+      .get("https://localhost:7267/User/GetAll")
+      .then((response) => {
+        console.log(response.data);
         setUsers(response.data);
       })
-      .catch(error => {
-        console.log('There was an error fetching the users!');
+      .catch((error) => {
+        console.log("There was an error fetching the users!");
       });
+  }
+  useEffect(() => {
+    getUsers();
   }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!user.firstName || !user.lastName){
+    if (!user.firstName || !user.lastName) {
       alert("Please provide a user.");
-    }
-    else{
+    } else {
       if (editId !== null) {
-        axios.put(`https://localhost:7267/User/UpdateUser/${editId}`, user)
+        axios
+          .put(`https://localhost:7267/User/UpdateUser/${editId}`, user)
           .then(() => {
-            const updatedTable = users.map(item =>
-              item.id === editId
-                ? { ...item, ...user }
-                : item
+            const updatedTable = users.map((item) =>
+              item.id === editId ? { ...item, ...user } : item
             );
             setUsers(updatedTable);
             setEditId(null);
-            setUser({ firstName: '', lastName: '', sex: '', dateOfBirth: ''});
-            console.log('User successfully updated.');
+            setUser({ firstName: "", lastName: "" });
+            console.log("User successfully updated.");
           })
-          .catch(error => {
-            console.log('There was an error updating the user!', error);
+          .catch((error) => {
+            console.log("There was an error updating the user!", error);
           });
-      } 
-      else {
-        axios.post('https://localhost:7267/User', user)
-        .then((response) => {
-          const newData = response.data; 
-          const updatedTable = [...users, newData];
-          setUsers(updatedTable);
-          setUser({ firstName: '', lastName: '', sex: '', dateOfBirth: '' });
-          console.log('User successfully added.');
-        })
-        .catch(error => {
-          console.log('There was an error adding the user!', error);
-        });
-       }
+      } else {
+        axios
+          .post("https://localhost:7267/User", user)
+          .then((response) => {
+            const newData = response.data;
+            const updatedTable = [...users, newData];
+            setUsers(updatedTable);
+            setUser({ firstName: "", lastName: "" });
+            console.log("User successfully added.");
+          })
+          .catch((error) => {
+            console.log("There was an error adding the user!", error);
+          });
+      }
     }
   };
 
-  const handleEdit = (id, firstName, lastName, sex, dateOfBirth) => {
+  const handleEdit = (id, firstName, lastName) => {
+    debugger;
     setEditId(id);
-    setUser({ firstName, lastName, sex, dateOfBirth}); 
+    setUser({ firstName, lastName });
   };
 
   function handleDelete(id) {
     try {
+      debugger;
       axios
         .delete(`https://localhost:7267/User/DeleteUser/${id}`)
         .then((response) => {
           console.log(response.data);
+          getUsers();
         });
       console.log("User successfully deleted!");
     } catch (error) {
@@ -82,18 +86,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <div className="LoginDiv">
-            <h1>Welcome to MyReactApp</h1>
-            <h2>{editId ? 'Edit user info:' : 'Enter user info:'}</h2>
-            <InputBar inputValue={user.firstName} setInputValue={(value) => setUser({ ...user, firstName: value })} placeholder="First Name" />
-            <InputBar inputValue={user.lastName} setInputValue={(value)=> setUser({ ...user, lastName: value })} placeholder="Last Name" />
-            <InputBar inputValue={user.sex} setInputValue={(value)=> setUser({ ...user, sex: value })} placeholder="Sex" />
-            <InputBar inputValue={user.dateOfBirth} setInputValue={(value)=> setUser({ ...user, dateOfBirth: value })} placeholder="Date of Birth" />
-            <Button />
-            <Table data={users} onEdit={handleEdit} onDelete={handleDelete}/>
-          </div>
-        </form>
+        <div className="LoginDiv">
+          <h1>Welcome to MyReactApp</h1>
+          <h2>{editId ? "Edit user info:" : "Enter user info:"}</h2>
+          <InputBar
+            inputValue={user.firstName}
+            setInputValue={(value) => setUser({ ...user, firstName: value })}
+            placeholder="First Name"
+          />
+          <InputBar
+            inputValue={user.lastName}
+            setInputValue={(value) => setUser({ ...user, lastName: value })}
+            placeholder="Last Name"
+          />
+          <Button handleSubmit={handleSubmit} />
+          <Table data={users} onEdit={handleEdit} onDelete={handleDelete} />
+        </div>
       </header>
     </div>
   );
--- FrontendNew/reactintroduction/src/Table.js
+++ FrontendNew/reactintroduction/src/Table.js
@@ -1,44 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export function Table({ data, onEdit, onDelete }) {
   return (
-    <>
-    <form>
-    <div className='Table'>
-    <table border="1">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Sex</th>
-          <th>Date of birth</th>
-          <th>Actions</th> {}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row) => (
-          <tr key={row.id}>
-            <td>{row.id}</td>
-            <td>{row.firstName}</td>
-            <td>{row.lastName}</td>
-            <td>{row.sex}</td>
-            <td>{row.dateOfBirth}</td>
-            <td>
-              <button onClick={() => onEdit(row.id, row.firstName, row.lastName, row.sex, row.dateOfBirth)}>
-                Edit
-              </button>
-              <button onClick={() => onDelete(row.id)}>
-                Delete
-              </button>
-            </td>
+    <div className="Table">
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Actions</th> {}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <tr key={row.id}>
+              <td>{row.id}</td>
+              <td>{row.firstName}</td>
+              <td>{row.lastName}</td>
+              <td>
+                <button
+                  onClick={() => onEdit(row.id, row.firstName, row.lastName)}
+                >
+                  Edit
+                </button>
+                <button onClick={() => onDelete(row.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-    </form>
-    </>
   );
 }
